6b33178e8d5ee7680d96b755c16c0d9ba0d8009d
Ilya <milyavskyfamily@gmail.com>
2022-04-26 22:48:10 -0400

    commit tree 6fa21a002a445e5def9cddf4c91df73f6b0ffa4a

--- methods.rb
+++ methods.rb
@@ -1,1 +1,36 @@
 # Your code here!
+
+def greet_programmer()
+    puts "Hello, programmer!"
+end
+
+def greet(name)
+    puts "Hello, #{name}!"
+end
+
+greet("Naureen")
+greet("Jimmy")
+
+def greet_with_default(name="programmer")
+    puts "Hello, #{name}!"
+end 
+
+greet_with_default('Naureen')
+
+def add num1, num2
+    num1 + num2
+end
+
+add(1,2)
+
+nill=nil
+
+def halve (num)
+    
+     return nil unless num.class == Integer
+
+        num/2
+end
+
+halve(6)
+
